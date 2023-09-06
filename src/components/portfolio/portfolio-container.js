33a5146ff8e2662c80e1f385ee3cb0445f1ceaf3
Daroch <daroch314@gmail.com>
2023-09-06 02:48:09 +0200

    added custom function an passed props to child component

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-import portfolioItem from "./portfolio-item";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
@@ -8,11 +7,17 @@ export default class PortfolioContainer extends Component {
         super();
         console.log("Portfolio Container is rendered");
     }
+    portfolioItems(){
+        const data=["Frodo","Gandalf","Legolas","Aragorn"];
+        return data.map(item => {
+            return <PortfolioItem title={item}/>
+        })
+    }
     render() {
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         );
     }
