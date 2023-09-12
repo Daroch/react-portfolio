46d43814a2651c6a0a78fe35d85fe12bbb416610
Daroch <daroch314@gmail.com>
2023-09-12 23:31:50 +0200

    added image field

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,9 +2,10 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export default function(props) {
-  const {id, name, url, thumb_inage_url, logo, description} = props.item;
+  const {id, name, url, thumb_image_url, logo, description} = props.item;
   return (
     <div>
+      <img src={thumb_image_url} />
       <h3>{name}</h3>
       <h3>{description}</h3>
       <h4><Link to={`/portfolio/${id}`}>{name}</Link></h4>
