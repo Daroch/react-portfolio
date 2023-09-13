8c805f24fbafe6d0ca9ac19f05149746fb297799
Daroch <daroch314@gmail.com>
2023-09-13 04:26:40 +0200

    added background images

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -5,7 +5,10 @@ export default function(props) {
   const {id, name, url, thumb_image_url, logo, description} = props.item;
   return (
     <div className="portfolio-item-wrapper">
-      <img src={thumb_image_url} />
+      <div className="portfolio-img-background" style={{
+        backgroundImage: "url(" + thumb_image_url + ")"
+      }} />
+      <img src={logo} />
       <h3>{name}</h3>
       <h3>{description}</h3>
       <h4><Link to={`/portfolio/${id}`}>{name}</Link></h4>
