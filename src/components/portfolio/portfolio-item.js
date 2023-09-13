c261a96b7afe045649a352024f948e41eda66ba5
Daroch <daroch314@gmail.com>
2023-09-13 19:23:30 +0200

    added styles in portfolio item, center logo and text

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,16 +2,18 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export default function(props) {
-  const {id, name, url, thumb_image_url, logo, description} = props.item;
+  const {id, name, url, thumb_image_url, logo_url, description} = props.item;
   return (
     <div className="portfolio-item-wrapper">
       <div className="portfolio-img-background" style={{
         backgroundImage: "url(" + thumb_image_url + ")"
       }} />
-      <img src={logo} />
-      <h3>{name}</h3>
-      <h3>{description}</h3>
-      <h4><Link to={`/portfolio/${id}`}>{name}</Link></h4>
+      <div className="image-text-wrapper">
+        <div className="logo-wrapper">
+        <img src={logo_url} />
+        </div>
+        <div className="subtitle">{description}</div>
+      </div>
     </div>
   );
 }
