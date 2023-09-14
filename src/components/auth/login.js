fb1f49c993e27ec80f2b1b5be83834227fb1d186
Daroch <daroch314@gmail.com>
2023-09-14 14:05:34 +0200

    event listeners on form inputs change and submit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log("Change State", event);
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log("Submit Form", event);
+    }
+
     render() {
         return (
             <div>
                 <h1>Login to Access DashBoard</h1>
-                <form>
-                    <input type='text' />
-                    <input type='password' />
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                    type="text" 
+                    name="email" 
+                    placeholder="Your email" 
+                    value={this.state.email} 
+                    onChange={this.handleChange}
+                    />
+                    <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="Your password" 
+                    value={this.state.password} 
+                    onChange={this.handleChange}
+                    />
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         );
