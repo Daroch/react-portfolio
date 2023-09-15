0d0c5c16274a423fd8cab9065deb263761b0f974
Daroch <daroch314@gmail.com>
2023-09-15 12:39:19 +0200

    passing custom render with props to childs components

--- src/components/app.js
+++ src/components/app.js
@@ -17,6 +17,27 @@ import About from './pages/about';
 import NoMatch from './pages/no-match';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    }
+    this.handleSuccesfulLogin = this.handleSuccesfulLogin.bind(this);
+    this.handleUnSuccesfulLogin = this.handleUnSuccesfulLogin.bind(this);
+  }
+
+  handleSuccesfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+
+  handleUnSuccesfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
   render() {
     return (
       <div className='container'>
@@ -29,7 +50,16 @@ export default class App extends Component {
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
               <Route exact path="/portfolio/:slug" component={PortfolioDetails} />
-              <Route path="/auth" component={Auth} />
+              <Route 
+                path="/auth" 
+                render={props => (
+                  <Auth 
+                    {...props} 
+                    handleSuccesfulLogin={this.handleSuccesfulLogin} 
+                    handleUnSuccesfulLogin={this.handleUnSuccesfulLogin}
+                    />
+                )
+              } />
               <Route component={NoMatch} />
             </Switch>
           </div>
