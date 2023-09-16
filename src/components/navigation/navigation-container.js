0eadc16041b102f43aa0f1df26b5c2b315eed04b
Daroch <daroch314@gmail.com>
2023-09-16 21:39:30 +0200

    high order component to add router to a component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import {withRouter} from 'react-router';
+
 import {NavLink} from 'react-router-dom';
 
 const NavigationContainer = (props) =>{
@@ -7,30 +10,42 @@ const NavigationContainer = (props) =>{
             <div className="nav-link-wrapper">
                 <NavLink to={route} activeClassName="nav-link-active">{Linktext}</NavLink>
             </div>
-        )            
+        )
     }
-        return (
-            <div className="nav-wrapper">
-                <div className="left-side">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-                    </div>
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/about" activeClassName="nav-link-active">About</NavLink>
-                    </div>
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-                    </div>
-                    {props.loggedInStatus === "LOGGED_IN" ? dinamycLink("/blog","Blog") : null}
-                    <div className="nav-link-wrapper">
-                        {false ? <NavLink to="/add_post" activeClassName="nav-link-active">Add Post</NavLink> : null}
-                    </div>
-                </div>
-                <div className="right-side">
-                    Daroch
-                </div>
 
-            </div>
-        );
+    const handleSignOut = () => {
+        axios.delete("https://api.devcamp.space/logout", {withCredentials: true}).then(response => {
+            if (response.status === 200){
+                props.history.push("/");
+                props.handleSuccesfulLogout();
+            }
+            return response.data;
+        }).catch(error => {
+            console.log("Error User SignOut", error);
+        })
     }
-    export default NavigationContainer;
+    return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to="/about" activeClassName="nav-link-active">About</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                </div>
+                {props.loggedInStatus === "LOGGED_IN" ? dinamycLink("/blog","Blog") : null}
+                <div className="nav-link-wrapper">
+                    {false ? <NavLink to="/add_post" activeClassName="nav-link-active">Add Post</NavLink> : null}
+                </div>
+            </div>
+            <div className="right-side">
+                Daroch {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null}
+            </div>
+
+        </div>
+    );
+}
+export default withRouter(NavigationContainer);
