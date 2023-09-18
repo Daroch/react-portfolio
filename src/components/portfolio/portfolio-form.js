f1fa39d224ba45627634fbafe2bb5c70d8648168
Daroch <daroch314@gmail.com>
2023-09-18 19:46:44 +0200

    added form fields and handlerChange function to update the state

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,10 +1,92 @@
 import React, { Component } from 'react';
 
 export default class PortfolioForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            description: "",
+            url: "",
+            category: "",
+            position: "",
+            thumb_image: "",
+            banner_image: "",
+            logo: ""
+        }
+
+        this.handlerChange = this.handlerChange.bind(this);
+    }
+
+    handlerChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render() {
         return (
             <div>
-                Form fields...
+                <form>
+                    <div>
+                        <input
+                        type="text"
+                        name="name"
+                        placeholder="Project Name"
+                        value={this.state.name}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="description"
+                        placeholder="Project Description"
+                        value={this.state.description}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="url"
+                        placeholder="Project Url"
+                        value={this.state.url}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="category"
+                        placeholder="Category"
+                        value={this.state.category}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="position"
+                        placeholder="Position"
+                        value={this.state.position}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="thumb_image"
+                        placeholder="Image Url"
+                        value={this.state.thumb_image}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="banner_image"
+                        placeholder="Banner"
+                        value={this.state.banner_image}
+                        onChange={this.handlerChange}
+                        />
+                        <input
+                        type="text"
+                        name="logo"
+                        placeholder="Logo"
+                        value={this.state.logo}
+                        onChange={this.handlerChange}
+                        />
+                    </div>
+                    <div><button type='submit'>Save</button></div>
+                </form>
             </div>
         );
     }
