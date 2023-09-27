5bc058b8cfef6b86224750c443e66d99997ace0b
Daroch <daroch314@gmail.com>
2023-09-27 19:35:05 +0200

    get blog items data from api and add to state

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+        blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this)
+;  }
+
+  getBlogItems() {
+    axios.get('https://daroch314.devcamp.space/portfolio/portfolio_blogs',
+    {withCredentials: true}
+    ).then(response => {
+        //console.log('GetBlogItems', response);
+        this.setState({
+            blogItems: response.data.portfolio_blogs
+        })
+    }).catch(error => {
+        console.log('Error getBlogItems', error);
+    });
   }
 
+  componentWillMount() {
+    this.getBlogItems();
+  }
   render() {
     return (
       <div>
         <div>
-            List of post
+            <h2>List of post</h2>
         </div>
         <div>
           <Link to="/about">Read more about myself</Link>
