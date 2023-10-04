71905efc2c7a615a883d7fd8387b2c18dccee485
Daroch <daroch314@gmail.com>
2023-10-04 12:40:50 +0200

    added basic state and handler to blog form

--- src/components/blog/blog-forms.js
+++ src/components/blog/blog-forms.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
 export default class BlogForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            content: "",
+            blog_status: "draft",
+            featured_image: ""
+        }
+
+        this.handleChangeBlogForm = this.handleChangeBlogForm.bind(this);
+    }
+
+    handleChangeBlogForm(event) {
+        console.log("handleChangeBlogForm", event);
+    }
     render() {
         return (
             <form>
-                <input type='text' />
-                <input type='text' />
+                <input 
+                name="title"
+                type="text"
+                placeholder="Title of your post"
+                value={this.state.title}
+                onChange={this.handleChangeBlogForm} />
+                <input 
+                name="content"
+                type="textarea"
+                placeholder="Content of your post"
+                value={this.state.content}
+                onChange={this.handleChangeBlogForm} />
 
                 <button>Save</button>
             </form>
